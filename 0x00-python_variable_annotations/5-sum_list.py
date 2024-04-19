62570065c23487dcdb82ff866a903943480a7717
imadovetch <ibouderoua63@gmail.com>
2024-04-19 01:14:33 +0100

    fix

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" def function sum_list """
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """ return sum of list number """
+
+    return sum(input_list)
